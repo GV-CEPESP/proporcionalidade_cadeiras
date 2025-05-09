d9b9da3da57ecb5f2efefc5a72b0ab928f03ea71
lgelape <lgelape@gmail.com>
2025-05-09 15:48:44 -0300

    upload relatorio

---
+++ codigos/dist_cadeiras.R
@@ -0,0 +1,476 @@
+################################################################################
+################################################################################
+
+##### Distribuicao de cadeiras na Camara dos Deputados brasileira
+
+# Limpa o environment, se necessario
+#rm(list = ls())
+
+# Pacotes
+library(dplyr)
+library(sidrar)
+library(janitor)
+library(readr)
+library(stringr)
+library(googlesheets4)
+library(ggplot2)
+library(tidyr)
+library(forcats)
+
+################################################################################
+
+##### Importacao de dados relevantes
+
+### Populacao (Censo-IBGE)
+
+# 2010
+populacao2010 <- get_sidra(136, geo = c("State")) |> 
+  clean_names() |> 
+  filter(ano == 2010,
+         cor_ou_raca == "Total") |> 
+  group_by(unidade_da_federacao) |> 
+  summarise(pop2010 = sum(valor, na.rm = T)) |> 
+  ungroup() |> 
+  select(unidade_da_federacao, pop2010) |> 
+  mutate(uf = toupper(unidade_da_federacao))
+
+# 2022
+populacao2022 <- get_sidra(4709, geo = c("State")) |> 
+  clean_names() |> 
+  filter(variavel == "População residente") |> 
+  select(unidade_da_federacao, valor) |> 
+  rename(pop2022 = valor) |> 
+  mutate(uf = toupper(unidade_da_federacao))
+
+### Eleitorado atual (2025, TSE)
+
+# # Baixa o arquivo de eleitorado atual
+# download.file(
+#   "https://cdn.tse.jus.br/estatistica/sead/odsele/perfil_eleitorado/perfil_eleitorado_ATUAL.zip",
+#   "dados/eleitorado_atual.zip")
+# 
+# # Descomprime o arquivo
+# unzip(
+#   "eleitorado_atual.zip",
+#   exdir = "dados/eleitorado_atual")
+#
+# # Apaga o arquivo zipado
+#file.remove("dados/eleitorado_atual.zip")
+
+# Importa o arquivo de eleitorado
+eleitorado <- read_csv2("dados/eleitorado_atual/perfil_eleitorado_ATUAL.csv",
+                        locale = locale(encoding = "latin1")) |> 
+  # Soma o n. de eleitores por UF
+  group_by(SG_UF) |> 
+  summarise(eleitorado2025 = sum(QT_ELEITORES_PERFIL, na.rm = T)) |> 
+  ungroup() |> 
+  clean_names()
+
+### Distribuicao de cadeiras
+
+# Atual
+magnitude <- read_csv(
+  "https://github.com/GV-CEPESP/cepespdata/raw/refs/heads/main/tabelas_auxiliares/dados/vagas_depfed_aux.csv",
+  locale = locale(encoding = "latin1")) |> 
+  select(NM_UE, SG_UF, VAGAS_2022) |> 
+  clean_names() |> 
+  rename(cadeiras_atual = vagas_2022)
+
+# Proposta aprovada pela Camara dos Deputados
+proposta_camara <- tibble(
+  sg_uf = magnitude$sg_uf,
+  cadeiras_atual = magnitude$cadeiras_atual) |> 
+  mutate(
+    dist_camara = case_when(
+      sg_uf == "CE" ~ cadeiras_atual + 1,
+      sg_uf == "GO" ~ cadeiras_atual + 1,
+      sg_uf == "MG" ~ cadeiras_atual + 1,
+      sg_uf == "PR" ~ cadeiras_atual + 1,
+      sg_uf == "AM" ~ cadeiras_atual + 2,
+      sg_uf == "MT" ~ cadeiras_atual + 2,
+      sg_uf == "RN" ~ cadeiras_atual + 2,
+      sg_uf == "PA" ~ cadeiras_atual + 4,
+      sg_uf == "SC" ~ cadeiras_atual + 4,
+      T ~ cadeiras_atual)) |> 
+  select(sg_uf, dist_camara)
+
+### Unifica todas as informacoes
+dados <- populacao2010 |> 
+  left_join(populacao2022) |> 
+  left_join(magnitude, by = c("uf" = "nm_ue")) |> 
+  left_join(eleitorado, by = "sg_uf") |> 
+  left_join(proposta_camara, by = "sg_uf") |> 
+  select(sg_uf, pop2010, pop2022, eleitorado2025, cadeiras_atual, dist_camara)
+
+################################################################################
+
+##### Calculo da distribuicao de cadeiras por diferentes metodos
+
+### Funcao para distribuir cadeiras
+
+maiores_medias_camara <- function(banco, n_cadeiras, populacao){
+  
+  ### Calcula os valores da primeira distribuicao de cadeiras
+  tabela <- banco |> 
+    mutate(
+      # Quociente populacional nacional
+      quociente = sum({{populacao}})/n_cadeiras,
+      # Quociente populacional estadual
+      dist1 = trunc({{populacao}}/quociente),
+      # Trava as cadeiras no maximo
+      vagas_qp = ifelse(dist1 > 70, 70, dist1),
+      vagas_media = 0)
+  
+  ### Descobre quais UFs atingiriam no maximo 8 cadeiras
+  
+  # Descobre o n. de cadeiras ainda a serem distribuidas
+  cadeiras_a_distribuir <- n_cadeiras - sum(tabela$vagas_qp)
+  
+  while(cadeiras_a_distribuir > 0){
+    
+    tabela <- tabela |> 
+      mutate(
+        # Calcula as medias
+        sobra_media = {{populacao}}/(vagas_qp + 1 + vagas_media),
+        # Retira SP da disputa pelas sobras
+        sobra_media = ifelse(sg_uf == "SP", 0, sobra_media),
+        # Da a cadeira da rodada para aquele que tem a maior media
+        vagas_media = ifelse(sobra_media == max(sobra_media), 
+                             vagas_media + 1, vagas_media))
+    
+    # Checa o n. de cadeiras que faltam distribuir
+    cadeiras_a_distribuir <- n_cadeiras - sum(tabela$vagas_qp) - sum(tabela$vagas_media, na.rm = T)
+    
+  }
+  
+  # Cria vetor com as UFs que ficam em no maximo 8 cadeiras
+  limite_70 <- tabela |> 
+    mutate(dist_final = vagas_qp + vagas_media) |> 
+    filter(dist_final >= 70) |> 
+    pull(sg_uf)
+  
+  # Cria vetor com as UFs que ficam em no maximo 8 cadeiras
+  limite_8 <- tabela |> 
+    mutate(dist_final = vagas_qp + vagas_media) |> 
+    filter(dist_final <= 8) |> 
+    pull(sg_uf)
+  
+  ### Distribui novamente as cadeiras, fixando as UFs com 8 ou 70
+  
+  # Primeira distribuicao
+  tabela <- banco |> 
+    mutate(
+      # Quociente populacional nacional
+      quociente = sum({{populacao}})/n_cadeiras,
+      # Quociente populacional estadual
+      dist1 = trunc({{populacao}}/quociente),
+      # Trava as cadeiras no maximo
+      vagas_qp = ifelse(sg_uf %in% limite_70, 70, dist1),
+      # Trava as cadeiras no minimo
+      vagas_qp = ifelse(sg_uf %in% limite_8, 8, vagas_qp),
+      vagas_media = 0)
+  
+  # Verifica quantas cadeiras a serem distribuidas
+  cadeiras_a_distribuir <- n_cadeiras - sum(tabela$vagas_qp)
+  
+  # Enquanto ainda houver cadeiras a serem distribuidas
+  while(cadeiras_a_distribuir > 0){
+    
+    tabela <- tabela |> 
+      mutate(
+        # Calcula as medias
+        sobra_media = {{populacao}}/(vagas_qp + 1 + vagas_media),
+        # Retira os estados com 8 ou 70 cadeiras da distribuicao
+        sobra_media = ifelse(sg_uf %in% limite_70, 0, sobra_media),
+        sobra_media = ifelse(sg_uf %in% limite_8, 0, sobra_media),
+        # Da a cadeira da rodada para aquele que tem a maior media
+        vagas_media = ifelse(sobra_media == max(sobra_media), 
+                             vagas_media + 1, vagas_media))
+    
+    # Checa o n. de cadeiras que faltam distribuir
+    cadeiras_a_distribuir <- n_cadeiras - sum(tabela$vagas_qp) - sum(tabela$vagas_media, na.rm = T)
+    
+  }
+  
+  # Calcula a distribuicao final, somando aquelas do quociente e da media
+  tabela <- tabela |> 
+    mutate(nova_dist = vagas_qp + vagas_media) |> 
+    select(sg_uf, nova_dist)
+  
+  return(tabela)
+  
+}
+
+# Guarda o resultado de diferentes estimativas numa lista
+estimativas <- list(
+  # N. cadeiras: 513 | Pop. ref: Censo-2010
+  maiores_medias_camara(dados, 513, pop2010) |> 
+    rename(dist_513_2010 = nova_dist),
+  # N. cadeiras: 513 | Pop. ref: Censo-2022
+  maiores_medias_camara(dados, 513, pop2022) |> 
+    rename(dist_513_2022 = nova_dist),
+  # N. cadeiras: 513 | Pop. ref: Eleitorado-2025
+  maiores_medias_camara(dados, 513, eleitorado2025) |> 
+    rename(dist_513_2025 = nova_dist),
+  # N. cadeiras: 531 | Pop. ref: Censo-2010
+  maiores_medias_camara(dados, 531, pop2010) |> 
+    rename(dist_531_2010 = nova_dist),
+  # N. cadeiras: 531 | Pop. ref: Censo-2022
+  maiores_medias_camara(dados, 531, pop2022) |> 
+    rename(dist_531_2022 = nova_dist),
+  # N. cadeiras: 531 | Pop. ref: Eleitorado-2025
+  maiores_medias_camara(dados, 531, eleitorado2025) |> 
+    rename(dist_531_2025 = nova_dist))
+
+# Une os resultados das estimativas em uma tabela
+estimativas <- estimativas[[1]] |> 
+  left_join(estimativas[[2]]) |> 
+  left_join(estimativas[[3]]) |> 
+  left_join(estimativas[[4]]) |> 
+  left_join(estimativas[[5]]) |> 
+  left_join(estimativas[[6]])  
+
+################################################################################
+
+##### Avalia a proporcionalidade dos resultados
+
+### Organiza os dados
+
+# Unifica os resultados das estimativas
+final <- left_join(dados, estimativas)
+
+# Calcula as proporcoes de cadeiras em cada distribuicao, e nas populacoes
+final_proporcoes <- final |>
+  mutate(
+    across(
+      c(-sg_uf), 
+      function(x) round_half_up(100*x/sum(x, na.rm = T), 2), 
+      .names = "prop_{.col}"))
+
+### Calcula metricas de desproporcionalidade
+
+# Funcao para calculo do indice de Gallagher
+gallagher <- function(prop_pop, prop_cadeiras){
+  
+  sqrt((1/2)*(sum((prop_cadeiras-prop_pop)^2)))
+  
+}
+
+# Calcula diferentes metricas de desproporcionalidade
+metricas <- final_proporcoes |> 
+  summarise(
+    # Indice de Gallagher
+    lsq_atual = gallagher(prop_cadeiras_atual, prop_pop2022),
+    lsq_camara = gallagher(prop_dist_camara, prop_pop2022),
+    lsq_513_elei = gallagher(prop_dist_513_2025, prop_pop2022),
+    lsq_513_2022 = gallagher(prop_dist_513_2022, prop_pop2022),
+    lsq_531_elei = gallagher(prop_dist_531_2025, prop_pop2022),
+    lsq_531_2022 = gallagher(prop_dist_531_2022, prop_pop2022))
+
+# Reorganiza a tabela de resultados de desproporcionalidade
+metricas_final <- tibble(
+  resultado = as.vector(t(metricas)[,1]),
+  medidas = names(metricas)) |> 
+  mutate(
+    estimativa = str_remove_all(medidas, "^(.*?)_"),
+    medida = str_extract(medidas, "^(.*?)_"),
+    medida = str_remove(medida, "_")) |> 
+  select(medida, estimativa, resultado) |> 
+  arrange(medida, resultado) 
+
+################################################################################
+
+##### Comparacao da desproporcionalidade por diferentes metodos
+
+metricas_final |> 
+  # Filtra as medidas/estimativas que queremos plotar
+  # filter(medida == "lsq",
+  #        estimativa %in% c("513_2022", "atual", "531_2022", "camara")
+  # ) |> 
+  # Cria labels para a variavel estimativa, para melhorar viz
+  mutate(estimativa = factor(
+    estimativa,
+    levels = c("513_2022", "513_elei", "atual", "531_2022", "camara", "531_elei"),
+    labels = c("Censo-2022\n(Cadeiras: 513)", "Eleitorado-2025\n(Cadeiras: 513)",
+               "Atual\n(Cadeiras: 513)", "Censo-2022\n(Cadeiras: 531)",
+               "Nova regra Câmara\n(Cadeiras: 531)", "Eleitorado-2025\n(Cadeiras: 531)")),
+    resultado = round_half_up(resultado, 2)) |> 
+  ggplot() +
+  geom_point(aes(x = estimativa, y = resultado)) +
+  geom_line(aes(x = estimativa, y = resultado, group = "valor")) +
+  geom_label(
+    aes(x = estimativa, y = resultado + 0.3, label = resultado),
+    size = 3) +
+  scale_y_continuous(limits = c(0, 7)) +
+  labs(
+    title = "Desproporcionalidade na distribuição de cadeiras\nna Câmara dos Deputados - simulações",
+    y = "Índice de Gallagher\n(Desproporcionalidade)",
+    caption = "Fonte: elaborado por Mesquita e Gelape (2025),\ncom base em dados do IBGE, TSE e Câmara dos Deputados.") +
+  theme_bw() +
+  theme(
+    plot.title = element_text(hjust = 0.5, size = 10), 
+    plot.caption = element_text(hjust = 0.5, size = 6), 
+    axis.title.x = element_blank(),
+    axis.title.y = element_text(size = 8),
+    axis.text = element_text(size = 7))
+#ggsave("relatorios/figuras/lsq_comparacao.png", width = 6, height = 4.5)
+
+### Perda/ganho de cadeiras por diferentes metodos
+
+# Calcula a diferenca de cadeiras em cada metodo
+diferencas <- final |> 
+  select(sg_uf, cadeiras_atual, dist_camara, dist_513_2022, dist_531_2022) |> 
+  pivot_longer(
+    cols = c(starts_with("dist")),
+    names_to = "tipo",
+    values_to = "cadeiras_novas") |> 
+  mutate(tipo = str_remove(tipo, "dist_"),
+         diferenca = cadeiras_novas - cadeiras_atual)
+
+# Plota o grafico
+diferencas |> 
+  # Elimina as UF que nao tem diferencas
+  filter(diferenca != 0) |> 
+  # Categoriza os que perderam/ganharam para colorir as barras
+  mutate(sg_uf = fct_rev(sg_uf),
+         ganho = ifelse(diferenca < 0, "Perda", "Ganho"),
+         ganho = factor(ganho, levels = c("Perda", "Ganho"))) |> 
+  # Cria labels para os rotulos
+  mutate(tipo = factor(
+    tipo,
+    levels = c("atual", "513_2022", "camara", "531_2022"),
+    labels = c("Distribuição\natual",  
+               "Censo-2022\n(Cadeiras: 513)", "Nova regra Câmara\n(Cadeiras: 531)", 
+               "Censo-2022\n(Cadeiras: 531)"))) |> 
+  ggplot() +
+  geom_col(aes(x = diferenca, y = sg_uf, fill = ganho), 
+           color = "black", linewidth = 0.2) +
+  geom_vline(xintercept = 0, linetype = "dashed", linewidth = 0.4) +
+  facet_wrap(~ tipo, nrow = 1) +
+  scale_x_continuous(breaks = seq(-4, 5, 1)) +
+  scale_fill_manual(values = c("darkred", "darkgreen")) +
+  guides(fill = "none") +
+  labs(
+    title = "Diferença no n. de cadeiras sob diferentes regras - simulações",
+    x = "N. de cadeiras",
+    caption = "Fonte: elaborado por Mesquita e Gelape (2025),\ncom base em dados do IBGE, TSE e Câmara dos Deputados.") +
+  theme_bw() +
+  theme(
+    plot.title = element_text(hjust = 0.5, size = 10), 
+    plot.caption = element_text(hjust = 0.5, size = 6),
+    axis.title.y = element_blank(),
+    axis.title.x = element_text(size = 8),
+    axis.text = element_text(size = 7),
+    panel.grid.major = element_line(linewidth = 0.25),
+    panel.grid.minor = element_blank()) 
+#ggsave("resultados/imagens/diferencas_cadeiras.jpg", width = 6, height = 4)
+
+################################################################################
+
+##### Avalia a proporcionalidade dos resultados somente daqueles com 8 < M < 70
+
+# Calcula proporcoes de cadeiras atual e do PLP, com base nesse universo de UF
+final_prop_variaveis <- final |>
+  select(sg_uf, pop2022, cadeiras_atual, dist_camara) |> 
+  mutate(
+    cadeiras_atual = case_when(
+      cadeiras_atual == 8 ~ NA,
+      cadeiras_atual == 70 ~ NA,
+      T ~ cadeiras_atual),
+    dist_camara = case_when(
+      dist_camara == 8 ~ NA,
+      dist_camara == 70 ~ NA,
+      T ~ dist_camara),
+    across(
+      c(-sg_uf), 
+      function(x) round_half_up(100*x/sum(x, na.rm = T), 2), 
+      .names = "prop_{.col}"))
+
+## Calcula desproporcionalidade nas UFs que 8 < M < 70
+
+# Atual
+desp_atual <- final_prop_variaveis |> 
+  select(sg_uf, cadeiras_atual, pop2022) |> 
+  filter(!is.na(cadeiras_atual)) |> 
+  mutate(prop_pop2022 = 100*pop2022/sum(pop2022),
+         prop_cadeiras_atual = 100*cadeiras_atual/sum(cadeiras_atual)) |> 
+  summarise(desproporcionalidade = gallagher(prop_cadeiras_atual, prop_pop2022)) |> 
+  mutate(n_cadeiras = 513, marco = "Atual", serie = NA)
+
+# PLP
+desp_nova <- final_prop_variaveis |> 
+  select(sg_uf, dist_camara, pop2022) |> 
+  filter(!is.na(dist_camara)) |> 
+  mutate(prop_pop2022 = 100*pop2022/sum(pop2022),
+         prop_dist_camara = 100*dist_camara/sum(dist_camara)) |> 
+  summarise(desproporcionalidade = gallagher(prop_dist_camara, prop_pop2022)) |> 
+  mutate(n_cadeiras = 531, marco = "Nova regra", serie = NA)
+
+# Calcula a desproporcionalidade para intervalo entre 500-650 cadeiras
+tabela_variaveis <- NULL
+
+for(cadeiras in 500:650){
+  
+  # Calcula a distribuicao de cadeiras
+  indice <- maiores_medias_camara(dados, cadeiras, pop2022) |> 
+    # Restringe as UFs com 8 < M < 70
+    filter(nova_dist > 8 & nova_dist < 70) |> 
+    left_join(final_prop_variaveis, by = "sg_uf") |> 
+    mutate(
+      prop_pop2022 = 100*pop2022/sum(pop2022),
+      prop_distribuicao = 100*nova_dist/sum(nova_dist)) |> 
+    summarise(desp = gallagher(prop_distribuicao, prop_pop2022)) |> 
+    pull()
+  
+  # Faz uma tabela da iteracao
+  iteracao <- tibble(
+    n_cadeiras = cadeiras,
+    desproporcionalidade = indice)
+  
+  # Empilha entre iteracoes
+  tabela_variaveis <- bind_rows(tabela_variaveis, iteracao)
+  
+}
+
+# Plota o grafico
+tabela_variaveis |> 
+  mutate(
+    # Cria variavel mostrando o n. de cadeiras da distribuicao atual e do PLP
+    marco = case_when(
+      n_cadeiras == 513 ~ "Distribuição TSE",
+      n_cadeiras == 531 ~ "Distribuição TSE",
+      T ~ NA),
+    serie = "simulada") |> 
+  bind_rows(desp_atual, desp_nova) |> 
+  ggplot() +
+  geom_line(aes(x = n_cadeiras, y = desproporcionalidade, group = serie),
+            color = "darkgrey") +
+  geom_point(aes(x = n_cadeiras, y = desproporcionalidade, 
+                 color = marco, shape = marco), size = 2) +
+  geom_hline(yintercept = 0.2400956, linetype = "dotted") +
+  scale_x_continuous(breaks = seq(500, 650, 10)) +
+  scale_y_continuous(limits = c(0.0, 1.8)) +
+  scale_color_manual(values = c("darkblue", "purple", "darkorange"), na.translate = F) +
+  scale_shape_manual(values = c(15, 16, 17), na.translate = F) +
+  labs(title = "Desproporcionalidade pelo n. de cadeiras - Simulações",
+       subtitle = "Excluindo UFs que teriam 8 ou 70 cadeiras",
+       x = "N. de cadeiras", y = "Desproporcionalidade\n(Índice de Gallagher)",
+       caption = "Fonte: elaborado por Mesquita e Gelape (2025),\ncom base em dados do IBGE, TSE e Câmara dos Deputados.") +
+  guides(color = guide_legend(title = NULL),
+         shape = guide_legend(title = NULL)) +
+  theme_bw() +
+  theme(
+    plot.title = element_text(hjust = 0.5, size = 10), 
+    plot.subtitle = element_text(hjust = 0.5, size = 8), 
+    plot.caption = element_text(hjust = 0.5, size = 6),
+    axis.title = element_text(size = 8),
+    axis.text = element_text(size = 7),
+    panel.grid.major = element_line(linewidth = 0.25),
+    panel.grid.minor = element_blank(),
+    legend.position = "bottom",
+    legend.box.spacing = unit(0.25, "cm"),
+    legend.key.height = unit(0.5, "cm"),
+    legend.key.width = unit(0.5, "cm"),
+    legend.text = element_text(size = 7),
+    legend.margin = margin(c(0, 0, 0, 0)))
+#ggsave("resultados/imagens/desproporcionalidade_cadeiras.jpg", width = 6, height = 4)
---
+++ relatorios/avaliacao_preliminar.md
@@ -0,0 +1,45 @@
+# Avaliação preliminar da proporcionalidade da distribuição de cadeiras na Câmara dos Deputados
+
+No último dia 06 de maio, a Câmara dos Deputados aprovou o PLP 177/23, com o objetivo de atender à decisão do STF na [ADO 38](https://portal.stf.jus.br/processos/detalhe.asp?incidente=5149458). Nela, o Tribunal decidiu que o Congresso Nacional estava em mora legislativa para a revisão periódica da proporcionalidade na relação deputado/população (prevista no art. 45, §1º da CF/88), e estabeleceu que o Congresso teria até 30 de junho de 2025 para sanar esta omissão. Caso ela não fosse sanada, a distribuição de cadeiras deveria ser feita pelo TSE, segundo metodologia definida quando da elaboração da Resolução-TSE n. 23.389/2013, e com base nos dados do Censo-2022.
+
+O [PLP 177/23](https://www.camara.leg.br/proposicoesWeb/prop_mostrarintegra?codteor=2900034&filename=Tramitacao-PLP%20177/2023) propõe um aumento no número de cadeiras das atuais 513 para 531, segundo uma distribuição fixada no Anexo do PLP. Além disso, o projeto prevê futuras revisões periódicas, baseadas nos dados do Censo (iniciando-se no próximo Censo, e não com os dados do Censo de 2022, segundo o art. 4º), a partir de uma metodologia análoga à utilizada para a distribuição de assentos nas eleições proporcionais brasileiras (art. 5º) -- mesma metodologia adotada pelo TSE na [Resolução n. 23.389/2013](https://www.migalhas.com.br/arquivos/2014/5/art20140528-01.pdf).
+
+Em sua decisão, o STF destaca que o objetivo da revisão é corrigir eventuais distorções na proporcionalidade da relação deputado/população. Neste documento, apresentamos resultados de uma avaliação de cenários de distribuição de cadeiras e sua eventual proporcionalidade.
+
+## Metodologia para distribuição de cadeiras
+
+Comparamos a [atual distribuição de cadeiras da Câmara dos Deputados](https://www2.camara.leg.br/a-camara/conheca/numero-de-deputados-por-estado) e aquela prevista no [anexo do PLP 177/23](https://www.camara.leg.br/proposicoesWeb/prop_mostrarintegra?codteor=2900034&filename=Tramitacao-PLP%20177/2023) com eventuais distribuições alternativas realizadas pelo método proposto no PLP 177/23 (art. 5º) e na [Resolução-TSE n. 23.389/2013](https://www.migalhas.com.br/arquivos/2014/5/art20140528-01.pdf). Todos os cálculos são apresentados no código [codigos/dist_cadeiras.R] deste repositório.
+
+Inicialmente, validamos nosso algoritmo de distribuição de cadeiras pela redistribuição atual de cadeiras segundo o Censo-2010, conforme realizado pelo TSE em 2013. *Uma vez que nossos resultados finais correspondem ao declarado pelo TSE, adotamos o mesmo algoritmo para a redistribuição de cadeiras em diferentes cenários*.
+
+### Indicador para avaliação da proporcionalidade
+
+A dificuldade de mensuração da proporcionalidade é uma [questão notória na literatura especializada](https://doi.org/10.1093/oxfordjournals.pan.a029822). Para avaliar a proporcionalidade da distribuição de cadeiras, adaptamos o [índice de Gallagher](https://cepesp.fgv.br/saiba-mais-sobre-indicadores), também conhecido como índice dos mínimos quadrados (*least squares index*), proposto pelo cientista político Michael Gallagher em [artigo publicado na revista *Electoral Studies*](https://doi.org/10.1016/0261-3794(91)90004-c). Este índice é calculado da seguinte forma:
+
+$$
+\text{LSq} = \sqrt{ \frac{1}{2} \sum_{i=1}^{n} (c_i - p_i)^2 }
+$$
+
+onde $m_i$ é o percentual de cadeiras destinada ao estado $i$ em uma distribuição; $p_i$ é a população do estado $i$; e $n$ é o total de unidades federativas.
+
+## Resultados
+
+Dentre as simulações já realizadas, avaliamos (i) a desproporcionalidade geral da distribuição de Cadeiras na Câmara segundo diferentes populações de referência; (ii) o número de cadeiras a mais/menos por UF, em relação à distribuição atual, e a partir de diferentes regras de distribuição; e (iii) a desproporcionalidade da distribuição de cadeiras somente para as unidades federativas em que a magnitude (número de cadeiras) está entre 9 e 69.
+
+### Desproporcionalidade geral
+
+Na figura abaixo, vemos que, considerando toda a Câmara dos Deputados, se usarmos as distribuições de 513 cadeiras, seja pelo Censo-2022 ou pelo Eleitorado-2025 (TSE), teríamos uma distribuição mais proporcional do que a atual, ou aquelas produzidas com a distribuição de 531 cadeiras.
+
+![](/relatorios/figuras/lsq_comparacao.png)
+
+### Cadeiras adicionadas/removidas por UF
+
+Na figura abaixo, por sua vez, observamos como uma distribuição pela população censitária de 2022, a distribuição de 531 leva a menores perdas absolutas de cadeiras por alguns estados, como Rio de Janeiro, Bahia, Rio Grande do Sul, Piauí, Paraíba e Pernambuco, quando comparados às 513 cadeiras. Além disso, em relação à distribuição proposta no PLP 177/23, alguns estados como Ceará, Goiás, Minas Gerais, Mato Grosso, Pará e Santa Catarina ganhariam mais cadeiras -- já o Rio Grande do Norte ganharia uma cadeira a menos.
+
+![](/relatorios/figuras/lsq_comparacao.png)
+
+### Desproporcionalidade, excetuando UFs com números mínimo e máximo de deputados
+
+Por fim, avaliamos a desproporcionalidade considerando somente o universo de cadeiras e de UFs uma vez excluídas aquelas que têm o número mínimo (8) ou máximo (70) de cadeiras -- isto é $8 < M < 70$. Para tanto, simulamos diferentes tamanhos de Câmara dos Deputados (entre 500 e 650 cadeiras). Vemos na figura abaixo como quaisquer distribuições baseadas nos dados Censitários e a partir da metodologia de atualização proposta no próprio PLP 177/23 são mais proporcionais que a distribuição atual e aquela proposta no PLP para entrada em vigor nas eleições de 2026. Porém, vale ressaltar que entre a distribuição atual e a nova, considerando-se somente as UFs em que $8 < M < 70$, a nova distribuição é menos desproporcional.
+
+![](/relatorios/figuras/desproporcionalidade_cadeiras.jpg)
